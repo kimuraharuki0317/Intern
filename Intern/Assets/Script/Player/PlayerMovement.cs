b8b420431a9914fde2b70a98df4626125d2bea5d
CCS-WIN-112\Haruki Kimura <harukimura0317@gmail.com>
2025-03-17 17:12:16 +0900

    フォルダ整理、プレイヤー制御用スクリプト作成

---
+++ Intern/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /// <summary>
+    /// プレイヤーの移動速度
+    /// </summary>
+    const float moveSpeed = 3.0f;
+
+    /// <summary>
+    /// プレイヤーのジャンプ力
+    /// </summary>
+    const float jumpPower = 6.0f;
+
+    /// <summary>
+    /// 接地確認コンポーネント
+    /// </summary>
+    CheckGround checkGround;
+
+    /// <summary>
+    /// Rigidbodyコンポーネント
+    /// </summary>
+    Rigidbody rb;
+
+    void Start()
+    {
+        checkGround = GetComponent<CheckGround>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // 移動ベクトル
+        Vector3 moveVector = Vector3.zero;
+
+        // カメラのTransform取得
+        Transform cameraTransform = Camera.main.transform;
+
+        // Wキー（前方移動）
+        if (Input.GetKey(KeyCode.W)){
+            moveVector += cameraTransform.forward;
+        }
+
+        // Sキー（後方移動）
+        if (Input.GetKey(KeyCode.S)){
+            moveVector -= cameraTransform.forward;
+        }
+
+        // Dキー（右移動）
+        if (Input.GetKey(KeyCode.D)){
+            moveVector += cameraTransform.right;
+        }
+
+        // Aキー（左移動）
+        if (Input.GetKey(KeyCode.A)){
+            moveVector -= cameraTransform.right;
+        }
+
+        if(moveVector!=Vector3.zero)
+        {
+            moveVector.y = 0;
+
+            //移動方向に向ける
+            transform.forward = moveVector;
+
+            //実際に移動させる
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+
+
+        // スペースキー押下かつ接地していたらジャンプ
+        if (Input.GetKeyDown(KeyCode.Space) && checkGround.HitGround())
+        {
+            rb.AddForce(jumpPower * Vector3.up, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Intern/Assets/Script/Utility/CheckGround.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    /// <summary>
+    /// 接地判定用コライダー
+    /// </summary>
+    [SerializeField]
+    BoxCollider groundCheckDistance;
+
+    /// <summary>
+    /// 接地しているか
+    /// </summary>
+    bool hitGround = false;
+
+    /// <summary>
+    /// 地面オブジェクトのタグ
+    /// </summary>
+    const string groundTag = "Ground";
+
+    /// <summary>
+    /// 地面に接触しているかを返す
+    /// </summary>
+    /// <returns>地面に接触しているか(bool)</returns>
+    public bool HitGround()
+    {
+        return hitGround;
+    }
+
+    /// <summary>
+    /// 地面のタグを持つコライダーが入ってきたらhitGroundをtrueにする
+    /// </summary>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == groundTag){
+            hitGround = true;
+        }
+    }
+
+    /// <summary>
+    /// 地面のタグを持つコライダーが出たらhitGroundをtrueにする
+    /// </summary>
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == groundTag){
+            hitGround = false;
+        }
+    }
+}
